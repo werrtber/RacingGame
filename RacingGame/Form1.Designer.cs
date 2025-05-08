5d6390c43abb805be3ab66b64feaff06ed01e682
Tsvyk Yurii <werrtber16@gmail.com>
2025-05-08 20:07:44 +0300

    Add project files.

---
+++ RacingGame/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace RacingGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.loseText = new System.Windows.Forms.Label();
+            this.buttonRestart = new System.Windows.Forms.Button();
+            this.coin = new System.Windows.Forms.PictureBox();
+            this.enemy2 = new System.Windows.Forms.PictureBox();
+            this.enemy1 = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.bg1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelCoins = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.coin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bg1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 30;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // loseText
+            // 
+            this.loseText.AutoSize = true;
+            this.loseText.BackColor = System.Drawing.Color.Red;
+            this.loseText.Font = new System.Drawing.Font("Microsoft YaHei UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loseText.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.loseText.Location = new System.Drawing.Point(393, 276);
+            this.loseText.Name = "loseText";
+            this.loseText.Size = new System.Drawing.Size(313, 52);
+            this.loseText.TabIndex = 5;
+            this.loseText.Text = "Ти програв :((";
+            // 
+            // buttonRestart
+            // 
+            this.buttonRestart.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonRestart.Location = new System.Drawing.Point(455, 415);
+            this.buttonRestart.Name = "buttonRestart";
+            this.buttonRestart.Size = new System.Drawing.Size(197, 41);
+            this.buttonRestart.TabIndex = 6;
+            this.buttonRestart.Text = "Грати знову";
+            this.buttonRestart.UseVisualStyleBackColor = true;
+            this.buttonRestart.Click += new System.EventHandler(this.buttonRestart_Click);
+            // 
+            // coin
+            // 
+            this.coin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.coin.Image = global::RacingGame.Properties.Resources.coin;
+            this.coin.Location = new System.Drawing.Point(450, -550);
+            this.coin.Name = "coin";
+            this.coin.Size = new System.Drawing.Size(72, 70);
+            this.coin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.coin.TabIndex = 7;
+            this.coin.TabStop = false;
+            // 
+            // enemy2
+            // 
+            this.enemy2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.enemy2.Image = global::RacingGame.Properties.Resources.car2;
+            this.enemy2.Location = new System.Drawing.Point(753, -400);
+            this.enemy2.Name = "enemy2";
+            this.enemy2.Size = new System.Drawing.Size(128, 128);
+            this.enemy2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemy2.TabIndex = 4;
+            this.enemy2.TabStop = false;
+            // 
+            // enemy1
+            // 
+            this.enemy1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.enemy1.Image = global::RacingGame.Properties.Resources.car2;
+            this.enemy1.Location = new System.Drawing.Point(422, -130);
+            this.enemy1.Name = "enemy1";
+            this.enemy1.Size = new System.Drawing.Size(128, 128);
+            this.enemy1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemy1.TabIndex = 3;
+            this.enemy1.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.player.Image = global::RacingGame.Properties.Resources.car;
+            this.player.Location = new System.Drawing.Point(753, 515);
+            this.player.Margin = new System.Windows.Forms.Padding(0);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(128, 128);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 1;
+            this.player.TabStop = false;
+            // 
+            // bg1
+            // 
+            this.bg1.Image = global::RacingGame.Properties.Resources.background;
+            this.bg1.Location = new System.Drawing.Point(0, 0);
+            this.bg1.Name = "bg1";
+            this.bg1.Size = new System.Drawing.Size(1100, 800);
+            this.bg1.TabIndex = 0;
+            this.bg1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::RacingGame.Properties.Resources.background;
+            this.pictureBox1.Location = new System.Drawing.Point(0, -660);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1100, 800);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelCoins
+            // 
+            this.labelCoins.AutoSize = true;
+            this.labelCoins.BackColor = System.Drawing.Color.Red;
+            this.labelCoins.Font = new System.Drawing.Font("Microsoft YaHei UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelCoins.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelCoins.Location = new System.Drawing.Point(12, 9);
+            this.labelCoins.Name = "labelCoins";
+            this.labelCoins.Size = new System.Drawing.Size(232, 52);
+            this.labelCoins.TabIndex = 8;
+            this.labelCoins.Text = "Монети: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1100, 800);
+            this.Controls.Add(this.labelCoins);
+            this.Controls.Add(this.coin);
+            this.Controls.Add(this.buttonRestart);
+            this.Controls.Add(this.loseText);
+            this.Controls.Add(this.enemy2);
+            this.Controls.Add(this.enemy1);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.bg1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.coin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bg1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox bg1;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox enemy1;
+        private System.Windows.Forms.PictureBox enemy2;
+        private System.Windows.Forms.Label loseText;
+        private System.Windows.Forms.Button buttonRestart;
+        private System.Windows.Forms.PictureBox coin;
+        private System.Windows.Forms.Label labelCoins;
+    }
+}
+
---
+++ RacingGame/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RacingGame
+{
+    public partial class Form1: Form
+    {
+        private int coinCount = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            loseText.Visible = false;
+            buttonRestart.Visible = false;
+            this.KeyPreview = true; 
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)Keys.Escape)
+                this.Close();
+        }
+
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            int speed = 10;
+            bg1.Top += speed;
+            pictureBox1.Top += speed;
+
+            int Carspeed = 12;
+            enemy1.Top += Carspeed;
+            enemy2.Top += Carspeed;
+
+            coin.Top += speed;
+
+            if (bg1.Top >= 650)
+            {
+                bg1.Top = 0;
+                pictureBox1.Top = -650;
+            }
+            int[] possiblePositionsCar = { 180, 300, 430, 560 };
+            Random rand = new Random();
+            Random rand2 = new Random();
+
+            if (enemy1.Top >= 650)
+            {
+                enemy1.Top = -130;
+                enemy1.Left = possiblePositionsCar[rand.Next(0, possiblePositionsCar.Length)];
+            }
+
+            if (enemy2.Top >= 650)
+            {
+                enemy2.Top = -250;
+                enemy2.Left = possiblePositionsCar[rand.Next(0, possiblePositionsCar.Length)];
+            }
+
+            if(enemy1.Bounds.IntersectsWith(player.Bounds) || enemy2.Bounds.IntersectsWith(player.Bounds))
+            {
+                timer.Enabled = false;
+                loseText.Visible = true;
+                buttonRestart.Visible = true;
+            }
+
+            if (coin.Top >= 650)
+            {
+                coin.Top = -100;
+                int[] possiblePositionsCoins = { 200, 320, 450, 580 };
+                coin.Left = possiblePositionsCoins[rand2.Next(0, possiblePositionsCoins.Length)];
+            }
+
+            if(player.Bounds.IntersectsWith(coin.Bounds))
+            {
+                coinCount++;
+                labelCoins.Text = "Монети: " + coinCount.ToString();
+                coin.Top = -100;
+                int[] possiblePositionsCoins = { 200, 320, 450, 580 };
+                coin.Left = possiblePositionsCoins[rand2.Next(0, possiblePositionsCoins.Length)];
+            }
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int speed = 10;
+            if((e.KeyCode == Keys.Left || e.KeyCode == Keys.A) && player.Left>180)
+            {
+                player.Left -= speed;
+            }
+            else if ((e.KeyCode == Keys.Right || e.KeyCode == Keys.D) && player.Right<660)
+            {
+                player.Left += speed;
+            }
+        }
+
+        private void buttonRestart_Click(object sender, EventArgs e)
+        {
+            enemy1.Top = -130;
+            enemy2.Top = -130;
+            loseText.Visible = false;
+            buttonRestart.Visible = false;
+            timer.Enabled = true;
+            coinCount = 0;
+            labelCoins.Text = "Монети: 0" ;
+            coin.Top = -100;
+        }
+
+        
+    }
+}
